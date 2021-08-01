8b0632dbd334012adf7c98532a6f2850d17e9348
RHYS <62794614+hy-js@users.noreply.github.com>
2021-08-01 11:49:35 +0000

    fixes original passwords

--- unhashed-passwords.sql
+++ unhashed-passwords.sql
@@ -1,5 +1,5 @@
-('27d6e73f-421f-43c2-9dc9-b13065c127a4', 'Darren', 'Brown', 'dkb1@gmail.com', 'testpass1'),
-('aec5379b-e4f3-4e5f-ac9b-bac3e3dc2e16', 'Donna', 'Karren', 'karen1010@gmail.com', 'KazDj1'),
-('b4e15a1c-63e8-4f3d-880e-07771cc21d4a', 'Adam', 'Driver', 'drivera2020@hotmail.com', 'Driverpass23'),
-('6b7410b3-75a3-4181-b841-ad60856561e4', 'Alicia', 'Test', 'test123@gmail.com', '$2a$10$biZOzo0E2iXBfM1CfZHObuN.fSu2VacSQXZqC8Q5waABQ/OY4526y'),
-('deb88fef-4757-4acc-84bc-38548cee7000', 'Ian', 'Carrol', 'melbian@gmail.com', '$2a$10$182arKiZfFTsMziKMdRGH.glT7dp0r6hIHTAR5ixG5Xc.Fn15nUxK');
+    ('27d6e73f-421f-43c2-9dc9-b13065c127a4', 'Darren', 'Brown', 'dkb1@gmail.com', 'testpass1'),
+    ('aec5379b-e4f3-4e5f-ac9b-bac3e3dc2e16', 'Donna', 'Karren', 'karen1010@gmail.com', 'KazDj1'),
+    ('b4e15a1c-63e8-4f3d-880e-07771cc21d4a', 'Adam', 'Driver', 'drivera2020@hotmail.com', 'Driverpass23'),
+    ('6b7410b3-75a3-4181-b841-ad60856561e4', 'Alicia', 'Test', 'test123@gmail.com', 'testpass1223'),
+    ('deb88fef-4757-4acc-84bc-38548cee7000', 'Ian', 'Carrol', 'melbian@gmail.com', 'eomelb123');
