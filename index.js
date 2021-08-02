88db5fd4d62fbd5ee1c4c3925ced803641fbddaf
Rhys <rhysadawson@gmail.com>
2021-08-02 16:24:12 +1000

    adds session cookies, updates login view to show session

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const db = require('./conn/conn')
 const exphbs = require('express-handlebars')
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
 
 
 const users = require('./routes/users')
@@ -19,14 +21,31 @@ app.engine('hbs', exphbs({
     extname: '.hbs'
 }))
 
+/// Session Middleware
+app.use(
+  session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 3600000 // 1 hour
+      // secure: false, // must be true if served via HTTPS
+    },
+  }));
+
 // Use 'users' and 'schedules' routes w/ Express Router
 app.use("/users", users)
 app.use("/schedules", schedules)
 app.use("/login", login)
 
+
+  
+
 // Home route
 app.get('/', async (req, res) => {
+    if (!req.session.loggedIn) return res.redirect('/login')
     const allScheds = await db.any("SELECT * FROM schedules LEFT JOIN users ON schedules.user_id = users.user_id")
+    console.log(req.session)
     res.render('index', {allScheds})
 })
 
--- routes/login.js
+++ routes/login.js
@@ -2,52 +2,28 @@ const express = require('express');
 const router = express.Router();
 const db = require('../conn/conn');
 const bcrypt = require('bcrypt');
-const session = require('express-session');
-const { v4: uuidv4 } = require('uuid');
-
-const app = express();
 
 // TODO: [RD] change send to render and effect sessions
-
-// Session Middleware
-app.use(
-  session({
-    name: 'Login',
-    secret: uuidv4(),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 60000, // 1 min
-      // secure: false, // must be true if served via HTTPS
-    },
-  })
-);
+// TODO: [RD] implement log out with session.destroy() and stay logged in check
 
 router
   .route('/')
   .get((req, res) => {
-    res.render('login');
+    if (!req.session.loggedIn) return res.render('login', {title: 'Please log in', showForm: true});
+    res.render('login', {title: 'Already logged in', showForm: false})
   })
   .post((req, res) => {
-
     db.oneOrNone('SELECT * FROM users WHERE email = $1', [req.body.email.toLowerCase(),])
-      // Check if user exists in DB
-      .then((isUser) => {
-        if (!isUser) {
-          return res.send('Invalid User');
-        }
-        // Compare given password to user's pw in DB
-        bcrypt.compare(req.body.password, isUser.password, function (err, result) {
-          if (result) {
-            res.send('Successful login!');
-          } else {
-            res.send('Invalid details');
-          }
-        });
+      .then(async (user) => {
+        if (!user) return console.log("Invalid details")
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) return console.log("Invalid details")
+        if (validPassword){
+          req.session.loggedIn = true; 
+          res.redirect('/')}
       })
       .catch((e) => {
         console.log(e);
-        res.send('Error');
       });
   });
 
